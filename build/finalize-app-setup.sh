84b573d6a8f4ae10c1d81e1e42be9f197d5bc3da
Steven Nuñez <octosteve@github.com>
2023-06-12 21:31:52 +0000

    Import from incursion branch

---
+++ build/finalize-app-setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo update-rc.d redis-server enable
+
+ruby -rerb -e 'puts ERB.new(File.read(".devcontainer/domain.yml.erb")).result(binding)' > config/domain.yml
+cp config/database.yml.codespaces config/database.yml
+cp config/security.yml.example config/security.yml
+cp .devcontainer/cache_store.yml config/cache_store.yml
+cp .devcontainer/redis.yml config/redis.yml
+cp .devcontainer/dynamic_settings.yml config/dynamic_settings.yml
+bundle install
+yarn install
+
+bundle exec rake db:initial_setup
+bundle exec rails canvas:compile_assets_dev
